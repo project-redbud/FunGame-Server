45ed0d2d8bfdb9feddbeb6bcd45c6cd9c776203c
milimoe <mili@wrss.org>
2023-07-16 13:21:09 +0800

    DataRequest添加Reg

--- FunGame.Server/Controllers/DataRequestController.cs
+++ FunGame.Server/Controllers/DataRequestController.cs
@@ -19,6 +19,7 @@ namespace Milimoe.FunGame.Server.Controller
         public DataRequestType LastRequest => _LastRequest;
 
         private string ForgetVerify = "";
+        private string RegVerify = "";
         private DataRequestType _LastRequest = DataRequestType.UnKnown;
 
         public DataRequestController(ServerModel server)
@@ -42,6 +43,13 @@ namespace Milimoe.FunGame.Server.Controller
 
                 case DataRequestType.Main_CreateRoom:
                     break;
+
+                case DataRequestType.Main_UpdateRoom:
+                    break;
+
+                case DataRequestType.Reg_GetRegVerifyCode:
+                    Reg(data, result);
+                    break;
                 
                 case DataRequestType.Login_GetFindPasswordVerifyCode:
                     ForgetPassword(data, result);
@@ -49,6 +57,12 @@ namespace Milimoe.FunGame.Server.Controller
 
                 case DataRequestType.Login_UpdatePassword:
                     UpdatePassword(data, result);
+                    break;
+
+                case DataRequestType.Room_GetRoomSettings:
+                    break;
+
+                case DataRequestType.Room_GetRoomPlayerCount:
                     break;
             }
 
@@ -191,6 +205,136 @@ namespace Milimoe.FunGame.Server.Controller
         {
             _LastRequest = DataRequestType.Main_GetNotice;
             ResultData.Add("notice", Config.ServerNotice);
+        }
+
+        /// <summary>
+        /// 接收并验证注册验证码
+        /// </summary>
+        /// <param name="RequestData"></param>
+        /// <param name="ResultData"></param>
+        private void Reg(Hashtable RequestData, Hashtable ResultData)
+        {
+            string msg = "";
+            RegInvokeType returnType = RegInvokeType.None;
+            if (RequestData.Count >= 2)
+            {
+                ServerHelper.WriteLine("[" + ServerSocket.GetTypeString(SocketMessageType.DataRequest) + "] " + Server.GetClientName() + " -> Reg");
+                string username = DataRequest.GetHashtableJsonObject<string>(RequestData, UserQuery.Column_Username) ?? "";
+                string password = DataRequest.GetHashtableJsonObject<string>(RequestData, UserQuery.Column_Password) ?? "";
+                string email = DataRequest.GetHashtableJsonObject<string>(RequestData, UserQuery.Column_Email) ?? "";
+                string verifycode = DataRequest.GetHashtableJsonObject<string>(RequestData, RegVerifyCodes.Column_RegVerifyCode) ?? "";
+
+                // 如果没发验证码，就生成验证码
+                if (verifycode.Trim() == "")
+                {
+                    // 先检查账号是否重复
+                    SQLHelper.ExecuteDataSet(UserQuery.Select_IsExistUsername(username));
+                    if (SQLHelper.Result == SQLResult.Success)
+                    {
+                        ServerHelper.WriteLine(Server.GetClientName() + " 账号已被注册");
+                        returnType = RegInvokeType.DuplicateUserName;
+                    }
+                    else
+                    {
+                        // 检查邮箱是否重复
+                        SQLHelper.ExecuteDataSet(UserQuery.Select_IsExistEmail(email));
+                        if (SQLHelper.Result == SQLResult.Success)
+                        {
+                            ServerHelper.WriteLine(Server.GetClientName() + " 邮箱已被注册");
+                            returnType = RegInvokeType.DuplicateEmail;
+                        }
+                        else
+                        {
+                            // 检查验证码是否发送过
+                            SQLHelper.ExecuteDataSet(RegVerifyCodes.Select_HasSentRegVerifyCode(username, email));
+                            if (SQLHelper.Result == SQLResult.Success)
+                            {
+                                DateTime RegTime = (DateTime)SQLHelper.DataSet.Tables[0].Rows[0][RegVerifyCodes.Column_RegTime];
+                                string RegVerifyCode = (string)SQLHelper.DataSet.Tables[0].Rows[0][RegVerifyCodes.Column_RegVerifyCode];
+                                if ((DateTime.Now - RegTime).TotalMinutes < 10)
+                                {
+                                    ServerHelper.WriteLine(Server.GetClientName() + $" 十分钟内已向{email}发送过验证码：{RegVerifyCode}");
+                                }
+                                returnType = RegInvokeType.InputVerifyCode;
+                            }
+                            else
+                            {
+                                // 发送验证码，需要先删除之前过期的验证码
+                                SQLHelper.Execute(RegVerifyCodes.Delete_RegVerifyCode(username, email));
+                                RegVerify = Verification.CreateVerifyCode(VerifyCodeType.NumberVerifyCode, 6);
+                                SQLHelper.Execute(RegVerifyCodes.Insert_RegVerifyCode(username, email, RegVerify));
+                                if (SQLHelper.Result == SQLResult.Success)
+                                {
+                                    if (MailSender != null)
+                                    {
+                                        // 发送验证码
+                                        string ServerName = Config.ServerName;
+                                        string Subject = $"[{ServerName}] FunGame 注册验证码";
+                                        string Body = $"亲爱的 {username}， <br/>    感谢您注册[{ServerName}]，您的验证码是 {RegVerify} ，10分钟内有效，请及时输入！<br/><br/>{ServerName}<br/>{DateTimeUtility.GetDateTimeToString(TimeType.DateOnly)}";
+                                        string[] To = new string[] { email };
+                                        if (MailSender.Send(MailSender.CreateMail(Subject, Body, System.Net.Mail.MailPriority.Normal, true, To)) == MailSendResult.Success)
+                                        {
+                                            ServerHelper.WriteLine(Server.GetClientName() + $" 已向{email}发送验证码：{RegVerify}");
+                                        }
+                                        else
+                                        {
+                                            ServerHelper.WriteLine(Server.GetClientName() + " 无法发送验证码");
+                                            ServerHelper.WriteLine(MailSender.ErrorMsg);
+                                        }
+                                    }
+                                    else // 不使用MailSender的情况
+                                    {
+                                        ServerHelper.WriteLine(Server.GetClientName() + $" 验证码为：{RegVerify}，请服务器管理员告知此用户");
+                                    }
+                                    returnType = RegInvokeType.InputVerifyCode;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // 先检查验证码
+                    SQLHelper.ExecuteDataSet(RegVerifyCodes.Select_RegVerifyCode(username, email, verifycode));
+                    if (SQLHelper.Result == SQLResult.Success)
+                    {
+                        // 检查验证码是否过期
+                        DateTime RegTime = (DateTime)SQLHelper.DataSet.Tables[0].Rows[0][RegVerifyCodes.Column_RegTime];
+                        if ((DateTime.Now - RegTime).TotalMinutes >= 10)
+                        {
+                            ServerHelper.WriteLine(Server.GetClientName() + " 验证码已过期");
+                            msg = "此验证码已过期，请重新注册。";
+                            SQLHelper.Execute(RegVerifyCodes.Delete_RegVerifyCode(username, email));
+                        }
+                        else
+                        {
+                            // 注册
+                            if (RegVerify.Equals(SQLHelper.DataSet.Tables[0].Rows[0][RegVerifyCodes.Column_RegVerifyCode]))
+                            {
+                                ServerHelper.WriteLine("[Reg] UserName: " + username + " Email: " + email);
+                                SQLHelper.NewTransaction();
+                                SQLHelper.Execute(UserQuery.Insert_Register(username, password, email, Server.Socket?.ClientIP ?? ""));
+                                if (SQLHelper.Result == SQLResult.Success)
+                                {
+                                    msg = "注册成功！请牢记您的账号与密码！";
+                                    SQLHelper.Execute(RegVerifyCodes.Delete_RegVerifyCode(username, email));
+                                    SQLHelper.Commit();
+                                }
+                                else
+                                {
+                                    msg = "服务器无法处理您的注册，注册失败！";
+                                    SQLHelper.Rollback();
+                                }
+                            }
+                            else msg = "验证码不正确，请重新输入！";
+                        }
+                    }
+                    else if (SQLHelper.Result == SQLResult.NotFound) msg = "验证码不正确，请重新输入！";
+                    else msg = "服务器无法处理您的注册，注册失败！";
+                }
+            }
+            ResultData.Add("msg", msg);
+            ResultData.Add("type", returnType);
         }
     }
 }
