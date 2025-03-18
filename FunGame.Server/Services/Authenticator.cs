4cb82981c2d96adc8dae8d1ba503c1eb5c17667e
milimoe <mili@wrss.org>
2025-03-19 01:10:37 +0800

    Authenticator 改动

---
+++ FunGame.Server/Services/Authenticator.cs
@@ -0,0 +1,30 @@
+﻿using Milimoe.FunGame.Core.Api.Transmittal;
+using Milimoe.FunGame.Core.Interface.Base;
+using Milimoe.FunGame.Core.Library.Constant;
+
+namespace Milimoe.FunGame.Server.Services
+{
+    public class Authenticator(IServerModel Server, SQLHelper SQLHelper, MailSender? MailSender) : Core.Library.Common.Architecture.Authenticator(SQLHelper)
+    {
+        public TFA TFA = new(SQLHelper);
+
+        private readonly IServerModel Server = Server;
+        private readonly SQLHelper SQLHelper = SQLHelper;
+        private readonly MailSender? MailSender = MailSender;
+
+        public override bool AfterAuthenticator(AuthenticationType type, params object[] args)
+        {
+            if (type == AuthenticationType.Username && args[0] is string username)
+            {
+                // 添加2FA二次验证等
+            }
+            return true;
+        }
+
+        public override bool BeforeAuthenticator(AuthenticationType type, params object[] args)
+        {
+            // 添加人机验证或频繁验证等
+            return true;
+        }
+    }
+}
--- FunGame.Server/Services/TFA.cs
+++ FunGame.Server/Services/TFA.cs
@@ -1,8 +1,9 @@
-﻿using Milimoe.FunGame.Core.Api.Utility;
+﻿using Milimoe.FunGame.Core.Api.Transmittal;
+using Milimoe.FunGame.Core.Api.Utility;
 
 namespace Milimoe.FunGame.Server.Services
 {
-    public class TFA : TwoFactorAuthenticator
+    public class TFA(SQLHelper SQLHelper) : TwoFactorAuthenticator(SQLHelper)
     {
         public override bool IsAvailable(string username)
         {
